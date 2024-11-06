040f302394082fd88bea8faee6d4ee78612258d6
Krista Fryauff <kristafryauff@Kristas-MacBook-Pro.local>
2024-11-05 20:24:39 -0800

    ex: maxchar - 1

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+
+
+function maxChar(str) {
+  if (!str.length) return;
+  const charMap = {}
+  let currChar = str[0];
+  let currMax = 0
+
+  for (let char of str) {
+    const freq = (charMap[char] ?? 0) + 1
+    charMap[char] = freq
+
+    if (freq > currMax) {
+      currMax = freq
+      currChar = char
+    }
+  }
+
+  return currChar
+}
 
 module.exports = maxChar;
